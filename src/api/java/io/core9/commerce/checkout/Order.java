1ffe10e409ad972c1f5dddfc3ad5b2330fe1c04a
Mark Wienk <markwienk@gmail.com>
2014-04-21 19:41:47 +0200

    Added ID to Order

--- src/api/java/io/core9/commerce/checkout/Order.java
+++ src/api/java/io/core9/commerce/checkout/Order.java
@@ -3,6 +3,28 @@ package io.core9.commerce.checkout;
 import io.core9.commerce.cart.Cart;
 
 public interface Order {
+	
+	/**
+	 * Return the Order ID
+	 * @return
+	 */
+	String getId();
+	
+	/**
+	 * Set the Order ID
+	 * @param id
+	 */
+	void setId(String id);
+	
+	/**
+	 * Return the Cart associated with the order
+	 * @return
+	 */
 	Cart getCart();
+	
+	/**
+	 * Set the Cart associated with the order
+	 * @param cart
+	 */
 	void setCart(Cart cart);
 }
