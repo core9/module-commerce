92c21d3fe147a3ecfafce587a3f3e7daf8a3e79a
Mark Wienk <mark.wienk@core9.io>
2014-10-16 23:44:18 +0200

    cc

--- src/api/java/io/core9/commerce/checkout/old/CheckoutDataHandler.java
+++
@@ -1,9 +0,0 @@
-package io.core9.commerce.checkout.old;
-
-import io.core9.core.boot.BootStrategy;
-import io.core9.core.plugin.Core9Plugin;
-import io.core9.plugin.widgets.datahandler.DataHandlerFactory;
-
-public interface CheckoutDataHandler extends DataHandlerFactory<CheckoutDataHandlerConfig>, BootStrategy, Core9Plugin {
-	
-}
--- src/api/java/io/core9/commerce/checkout/old/FeedbackDataHandler.java
+++
@@ -1,8 +0,0 @@
-package io.core9.commerce.checkout.old;
-
-import io.core9.core.plugin.Core9Plugin;
-import io.core9.plugin.widgets.datahandler.DataHandlerFactory;
-
-public interface FeedbackDataHandler extends DataHandlerFactory<FeedbackDataHandlerConfig>, Core9Plugin {
-
-}
